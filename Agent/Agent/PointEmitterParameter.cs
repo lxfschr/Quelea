5efcce4324a5bf3db9c25c74da86c8fb64bf6e88
Alex Fischer <lxfschr@gmail.com>
2014-09-21 15:33:37 -0400

    First commit, Pt Emitter component loads

---
+++ Agent/Agent/PointEmitterParameter.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace Agent
+{
+    class PointEmitterDescription : IGH_InstanceDescription
+    {
+        private string name, nickname, category, subcategory, description;
+        private Guid guid;
+
+        public PointEmitterDescription(string name, string nickname, string description, string category, string subcategory)
+        {
+            this.name = name;
+            this.nickname = nickname;
+            this.description = description;
+            this.category = category;
+            this.subcategory = subcategory;
+            this.guid = new Guid("{239948e1-946b-4b33-ad73-bdf4b3472fcb}");
+        }
+
+        public string Category
+        {
+            get
+            {
+                return this.category;
+            }
+            set
+            {
+                this.category = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+            }
+        }
+
+        public bool HasCategory
+        {
+            get { return (this.category != null); }
+        }
+
+        public bool HasSubCategory
+        {
+            get { return (this.subcategory != null); }
+        }
+
+        public string InstanceDescription
+        {
+            get { return this.description; }
+        }
+
+        public Guid InstanceGuid
+        {
+            get { return this.guid; }
+        }
+
+        public IEnumerable<string> Keywords
+        {
+            get { return null; }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public void NewInstanceGuid(Guid UUID)
+        {
+            this.guid = UUID;
+        }
+
+        public void NewInstanceGuid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NickName
+        {
+            get
+            {
+                return this.nickname;
+            }
+            set
+            {
+                this.nickname = value;
+            }
+        }
+
+        public string SubCategory
+        {
+            get
+            {
+                return this.subcategory;
+            }
+            set
+            {
+                this.subcategory = value;
+            }
+        }
+
+        public bool Read(GH_IO.Serialization.GH_IReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Write(GH_IO.Serialization.GH_IWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class PointEmitterParameter : GH_Param<PointEmitterType>
+    {
+        public PointEmitterParameter() 
+            : base(new PointEmitterDescription("Point Emitter", "pEmit", "A Point Emitter", "Params", "Primitive"), GH_ParamAccess.item) { }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{49da6738-8a17-4296-9a72-dc7220b65a0e}"); }
+        }
+    }
+}
