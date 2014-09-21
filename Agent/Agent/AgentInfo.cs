a25efdc704bdd8cbfd3048482e804bf825604ad2
Alex Fischer <lxfschr@gmail.com>
2014-09-21 15:33:37 -0400

    First commit, Pt Emitter component loads

---
+++ Agent/Agent/AgentInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Agent
+{
+    public class AgentInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Spiral";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("8e2ef6d7-ac04-4dad-a95d-effdddc8a47b");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "Alex Fischer";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
---
+++ Agent/Agent/PointEmitterType.cs
@@ -0,0 +1,76 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace Agent
+{
+    public class PointEmitterType : GH_Goo<Point3d>
+    {
+
+        private Point3d pt;
+        bool continuousFlow;
+        int creationRate;
+        int numAgents;
+        
+        // Default Constructor. Defaults to continuous flow, creating a new Agent every timestep.
+        public PointEmitterType()
+        {
+            this.pt = Point3d.Origin;
+            this.continuousFlow = true;
+            this.creationRate = 1;
+            this.numAgents = 10;
+        }
+
+        // Constructor with initial values.
+        public PointEmitterType(Point3d pt, bool continuousFlow, int creationRate, int numAgents)
+        {
+            this.pt = pt;
+            this.continuousFlow = continuousFlow;
+            this.creationRate = creationRate;
+            this.numAgents = numAgents;
+        }
+
+        // Copy Constructor
+        public PointEmitterType(PointEmitterType ptEmitType)
+        {
+            this.pt = ptEmitType.pt;
+            this.continuousFlow = ptEmitType.continuousFlow;
+            this.creationRate = ptEmitType.creationRate;
+            this.numAgents = ptEmitType.numAgents;
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new PointEmitterType(this);
+        }
+
+        public override bool IsValid
+        {
+            get
+            {
+                return (this.pt.IsValid && this.creationRate > 0 && this.numAgents >= 0);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string origin = "Origin Point: " + pt.ToString() + "\n";
+            string continuousFlow = "ContinuousFlow: " + this.continuousFlow.ToString() + "\n";
+            string creationRate = "Creation Rate: " + this.creationRate.ToString() + "\n";
+            string numAgents = "Number of Agents: " + this.numAgents.ToString() + "\n";
+            return origin + continuousFlow + creationRate + numAgents;
+        }
+
+        public override string TypeDescription
+        {
+            get { return "A Point Emitter"; }
+        }
+
+        public override string TypeName
+        {
+            get { return "PointEmitterType"; }
+        }
+
+    }
+}
