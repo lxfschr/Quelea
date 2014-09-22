d9826b0c3dda0e6992181a1964ab1d2072b3d542
Alex Fischer <lxfschr@gmail.com>
2014-09-21 20:55:59 -0400

    engine creates animated particle list

---
+++ Agent/Agent/EngineComponent.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Agent
+{
+    public class EngineComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the Engine class.
+        /// </summary>
+        public EngineComponent()
+            : base("Engine", "Engine",
+                "Engine that runs the simulation.",
+                "Agent", "Agent")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Reset", "R", "Reset the scene?", GH_ParamAccess.item, true);
+            pManager.AddGenericParameter("Emitters", "E", "Emitters in scene.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Agents", "A", "Agents", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // First, we need to retrieve all data from the input parameters.
+            // We'll start by declaring variables and assigning them starting values.
+            Boolean reset = true;
+            PointEmitterType ptEmitter = new PointEmitterType();
+
+            // Then we need to access the input parameters individually. 
+            // When data cannot be extracted from a parameter, we should abort this method.
+            if (!DA.GetData(0, ref reset)) return;
+            if (!DA.GetData(1, ref ptEmitter)) return;
+
+            // We should now validate the data and warn the user if invalid data is supplied.
+
+            // We're set to create the output now. To keep the size of the SolveInstance() method small, 
+            // The actual functionality will be in a different method:
+            List<Point3d> agents = run(reset, ptEmitter);
+            //List<Point3d> agents = new List<Point3d>();
+
+            // Finally assign the spiral to the output parameter.
+            DA.SetDataList(0, agents);
+        }
+
+        List<Particle> pList = new List<Particle>();
+
+        private List<Point3d> run(Boolean reset, PointEmitterType emitter)
+        {
+
+            
+            List<Point3d> pts = new List<Point3d>();
+            
+            if (reset)
+            {
+                pList.Clear();
+            }
+            else
+            {
+
+                pList.Add(new Particle(new Vector3d(emitter.emit())));
+
+                for (int i = 0; i < pList.Count; i++)
+                {
+                    Particle getP = pList[i] as Particle;
+                    getP.run();
+                    Point3d pt = new Point3d(getP.location);
+                    pts.Add(pt);
+
+                    if (getP.isDead())
+                    {
+                        pts.Remove(pt);
+                    }
+                }
+            }
+
+            return pts;
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{7c241f1f-a156-42e8-9e16-4fc8ddab1d5a}"); }
+        }
+    }
+}
---
+++ Agent/Agent/Particle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace Agent
+{
+    class Particle
+    {
+        public Vector3d location = new Vector3d(0, 0, 0);
+        public Vector3d velocity = new Vector3d(0, 0, 0);
+        public Vector3d acceleration = new Vector3d(0, 0, 0);
+        public double lifespan;
+
+        public Particle(Vector3d l)
+        {
+            this.location = l;
+            //acceleration = new Vector3d(0, 0, 0);
+            this.acceleration = new Vector3d(0, 0, -0.05);
+            this.velocity = new Vector3d(0, 0, -0.1);
+            //Random random = new Random();
+            //velocity = new Vector3d(random.Next(-1, 1), random.Next(-2, 0), random.Next(-1, 0));
+            this.lifespan = 30.0;
+        }
+
+        public void update()
+        {
+            velocity = Vector3d.Add(velocity, acceleration);
+            location = Vector3d.Add(location, velocity);
+            lifespan -= 1.0;
+
+        }
+
+        public Boolean isDead()
+        {
+            if (lifespan <= 0.0) return true;
+            else return false;
+        }
+
+        public void run()
+        {
+            update();
+            //display();
+        }
+    }
+}
