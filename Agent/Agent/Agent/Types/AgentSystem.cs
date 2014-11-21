69fbb0750f9f7e24c0b4591f23b1ff4bf2ec4571
Alex Fischer <lxfschr@gmail.com>
2014-11-21 18:23:06 -0500

    saving before change to systems list

---
+++ Agent/Agent/Agent/Types/AgentSystem.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using System.Collections.ObjectModel;
+
+namespace Agent.Types
+{
+  public class AgentSystem : GH_Goo<Object>
+  {
+    private IList<AgentType> agents;
+    private AgentType[] agentsSettings;
+    private EmitterType[] emitters;
+    private ForceType[] forces;
+    private BehaviorType[] behaviors;
+    private EnvironmentType environment;
+    private int timestep;
+    private int nextIndex;
+
+    public AgentSystem()
+    {
+      this.agents = new List<AgentType>();
+      this.agentsSettings = new AgentType[] { new AgentType() };
+      this.emitters = new EmitterType[] { new EmitterPtType() };
+      this.environment = null;
+      this.forces = new ForceType[] { };
+      this.behaviors = new BehaviorType[] { };
+      this.timestep = 0;
+      this.nextIndex = 0;
+    }
+
+    public T.AgentSystem(AgentType[] agentsSettings, EmitterType[] emitters, 
+                           EnvironmentType environment, ForceType[] forces,
+                           BehaviorType[] behaviors)
+    {
+      this.agents = new List<AgentType>();
+      this.agentsSettings = agentsSettings;
+      this.emitters = emitters;
+      this.environment = environment;
+      this.forces = forces;
+      this.behaviors = behaviors;
+    }
+
+    public T.AgentSystem(T.AgentSystem system)
+    {
+      this.agents = new List<AgentType>(system.agents);
+      this.agentsSettings = system.agentsSettings;
+      this.emitters = system.emitters;
+      this.environment = system.environment;
+      this.forces = system.forces;
+      this.behaviors = system.behaviors;
+    }
+
+    public IList<AgentType> Agents
+    {
+      get
+      {
+        return this.agents;
+      }
+    }
+
+    public IEnumerable<AgentType> AgentsSettings
+    {
+      get
+      {
+        return this.agentsSettings;
+      }
+      set
+      {
+        this.agentsSettings = (AgentType[]) value;
+      }
+    }
+
+    public IEnumerable<EmitterType> Emitters
+    {
+      get
+      {
+        return this.emitters;
+      }
+      set // ToDo remove this
+      {
+        this.emitters = (EmitterType[]) value;
+      }
+    }
+
+    public IEnumerable<ForceType> Forces
+    {
+      get
+      {
+        return this.forces;
+      }
+      set // ToDo remove this
+      {
+        this.forces = (ForceType[]) value;
+      }
+    }
+
+    public IEnumerable<BehaviorType> Behaviors
+    {
+      get
+      {
+        return this.behaviors;
+      }
+      set
+      {
+        this.behaviors = (BehaviorType[]) value;
+      }
+    }
+    public EnvironmentType Environment
+    {
+      get
+      {
+        return this.environment;
+      }
+      set // ToDo remove this
+      {
+        this.environment = value;
+      }
+    }
+
+    public void applyForces(AgentType a)
+    {
+      foreach (ForceType force in this.forces)
+      {
+        Vector3d forceVec = force.calcForce(a, this.agents);
+        a.applyForce(forceVec);
+      }
+    }
+
+    public bool applyBehaviors(AgentType a)
+    {
+      bool behaviorsApplied = false;
+      foreach (BehaviorType behavior in this.behaviors)
+      {
+        if (behavior.applyBehavior(a, this))
+        {
+          behaviorsApplied = true;
+        }
+      }
+      return behaviorsApplied;
+    }
+
+    public void addAgent(EmitterType emitter)
+    {
+      Point3d emittionPt = emitter.emit();
+      AgentType agent;
+      if (environment != null)
+      {
+        Point3d refEmittionPt = environment.closestRefPoint(emittionPt);
+        agent = new AgentType(agentsSettings[nextIndex % agentsSettings.Length], emittionPt, refEmittionPt);
+      }
+      else
+      {
+        agent = new AgentType(agentsSettings[nextIndex % agentsSettings.Length], emittionPt);
+      }
+      agents.Add(agent);
+      nextIndex++;
+    }
+
+    public void run()
+    {
+      foreach (EmitterType emitter in emitters)
+      {
+        if (emitter.ContinuousFlow && (timestep % emitter.CreationRate == 0))
+        {
+          if ((emitter.NumAgents == 0) || (this.agents.Count < emitter.NumAgents))
+          {
+            addAgent(emitter);
+          }
+        }
+      }
+
+      for (int i = agents.Count - 1; i >= 0; i--)
+      {
+        AgentType a = agents[i];
+        
+        if (!applyBehaviors(a))
+        {
+          applyForces(a);
+        }
+        a.run();
+        if (environment != null)
+        {
+          a.RefPosition = environment.closestRefPointOnRef(a.RefPosition);
+          a.Position = environment.closestPointOnRef(a.RefPosition);
+        }
+        else
+        {
+          a.RefPosition = a.Position;
+          a.Position = a.RefPosition;
+        }
+        if (a.isDead())
+        {
+          agents.Remove(a);
+        }
+      }
+      timestep++;
+    }
+
+    public override bool Equals(object obj) //ToDo fix bugs in equals
+    {
+      // If parameter is null return false.
+      if (obj == null)
+      {
+        return false;
+      }
+
+      // If parameter cannot be cast to Point return false.
+      T.AgentSystem s = obj as T.AgentSystem;
+      if ((System.Object)s == null)
+      {
+        return false;
+      }
+
+      // Return true if the fields match:
+      return (this.emitters.Equals(s.emitters)) && 
+             (this.agentsSettings.Equals(s.agentsSettings)) &&
+             (this.environment.Equals(s.environment));
+    }
+
+    public bool Equals(T.AgentSystem s) //ToDo fix bugs in equals
+    {
+      // If parameter is null return false:
+      if ((object)s == null)
+      {
+        return false;
+      }
+
+      // Return true if the fields match:
+      return (this.emitters.Equals(s.emitters)) && 
+             (this.agentsSettings.Equals(s.agents)) &&
+             (this.environment.Equals(s.environment));
+    }
+
+    public override int GetHashCode() //ToDo fix bugs in equals
+    {
+      int agentHash = 1;
+      int emitterHash = 1;
+      foreach (AgentType agent in this.agentsSettings)
+      {
+        agentHash *= agent.GetHashCode();
+      }
+      foreach (EmitterType emitter in this.emitters)
+      {
+        emitterHash *= emitter.GetHashCode();
+      }
+      return agentHash ^ emitterHash ^ this.environment.GetHashCode();
+    }
+
+    public override IGH_Goo Duplicate()
+    {
+      return new T.AgentSystem(this);
+    }
+
+    public override bool IsValid
+    {
+      get
+      {
+        return (this.timestep >= 0) && (this.nextIndex >= 0);
+      }
+    }
+
+    public override string ToString() //ToDo
+    {
+      string agents = "Agents: " + this.agentsSettings.Length.ToString() + "\n";
+      string emitters = "Emitters: " + this.emitters.Length.ToString() + "\n";
+      string environment = "Environment: " + this.environment.ToString() + "\n";
+      return agents + emitters + environment;
+    }
+
+    public override string TypeDescription
+    {
+      get { return "An Agent System"; }
+    }
+
+    public override string TypeName
+    {
+      get { return "Agent System"; }
+    }
+  }
+}
---
+++ Agent/Agent/Agent/Types/AgentType.cs
@@ -0,0 +1,379 @@
+﻿using Grasshopper.Kernel.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rhino.Geometry;
+
+namespace Agent.Types
+{
+  public class AgentType : GH_Goo<Object>
+  {
+    private int lifespan;
+    private double mass;
+    private double bodySize;
+    private double maxSpeed;
+    private double maxForce;
+    private double visionAngle;
+    private double visionRadius;
+    private int historyLength;
+
+    private Point3d position;
+    private Vector3d velocity;
+    private Vector3d acceleration;
+
+    private Point3d refPosition;
+
+    public AgentType()
+    {
+      this.lifespan = 30;
+      this.mass = 1.0;
+      this.bodySize = 1.0;
+      this.maxSpeed = 0.5;
+      this.maxForce = 0.1;
+      this.visionAngle = 15.0;
+      this.visionRadius = 5.0;
+      this.historyLength = 0;
+      this.position = Point3d.Origin;
+      this.refPosition = Point3d.Origin;
+      this.velocity = Util.Random.RandomVector(-0.1, 0.1);
+      this.acceleration = Vector3d.Zero;
+    }
+
+    public AgentType(int lifespan, double mass, double bodySize,
+                     double maxSpeed, double maxForce, double visionAngle,
+                     double visionRadius, int historyLength)
+    {
+      this.lifespan = lifespan;
+      this.mass = mass;
+      this.bodySize = bodySize;
+      this.maxSpeed = maxSpeed;
+      this.maxForce = maxForce;
+      this.visionAngle = visionAngle;
+      this.visionRadius = visionRadius;
+      this.historyLength = historyLength;
+
+      this.position = Point3d.Origin;
+      this.refPosition = Point3d.Origin;
+      this.velocity = Util.Random.RandomVector(-0.1, 0.1);
+      this.acceleration = Vector3d.Zero;
+    }
+
+    public AgentType(int lifespan, double mass, double bodySize,
+                     double maxSpeed, double maxForce, double visionAngle,
+                     double visionRadius, int historyLength, Point3d position)
+    {
+      this.lifespan = lifespan;
+      this.mass = mass;
+      this.bodySize = bodySize;
+      this.maxSpeed = maxSpeed;
+      this.maxForce = maxForce;
+      this.visionAngle = visionAngle;
+      this.visionRadius = visionRadius;
+      this.historyLength = historyLength;
+
+      this.position = position;
+      this.refPosition = position;
+      this.velocity = Util.Random.RandomVector(-0.1, 0.1);
+      this.acceleration = Vector3d.Zero;
+    }
+
+    public AgentType(AgentType agent)
+    {
+      this.lifespan = agent.lifespan;
+      this.mass = agent.mass;
+      this.bodySize = agent.bodySize;
+      this.maxSpeed = agent.maxSpeed;
+      this.maxForce = agent.maxForce;
+      this.visionAngle = agent.visionAngle;
+      this.visionRadius = agent.visionRadius;
+      this.historyLength = agent.historyLength;
+
+      this.position = agent.position;
+      this.refPosition = agent.refPosition;
+      this.velocity = agent.velocity;
+      this.acceleration = agent.acceleration;
+    }
+
+    public AgentType(AgentType agent, Point3d position)
+    {
+      this.lifespan = agent.lifespan;
+      this.mass = agent.mass;
+      this.bodySize = agent.bodySize;
+      this.maxSpeed = agent.maxSpeed;
+      this.maxForce = agent.maxForce;
+      this.visionAngle = agent.visionAngle;
+      this.visionRadius = agent.visionRadius;
+      this.historyLength = agent.historyLength;
+
+      this.refPosition = position;
+      this.position = position;
+      this.velocity = this.velocity = Util.Random.RandomVector(-0.1, 0.1);
+      this.acceleration = agent.acceleration;
+    }
+
+    public AgentType(AgentType agent, Point3d position, Point3d refPosition)
+    {
+      this.lifespan = agent.lifespan;
+      this.mass = agent.mass;
+      this.bodySize = agent.bodySize;
+      this.maxSpeed = agent.maxSpeed;
+      this.maxForce = agent.maxForce;
+      this.visionAngle = agent.visionAngle;
+      this.visionRadius = agent.visionRadius;
+      this.historyLength = agent.historyLength;
+
+      this.position = position;
+      this.refPosition = refPosition;
+      this.velocity = this.velocity = Util.Random.RandomVector(-0.1, 0.1);
+      this.acceleration = agent.acceleration;
+    }
+
+    public int Lifespan
+    {
+      get
+      {
+        return this.lifespan;
+      }
+    }
+
+    public double Mass
+    {
+      get
+      {
+        return this.mass;
+      }
+    }
+
+    public double BodySize
+    {
+      get
+      {
+        return this.bodySize;
+      }
+    }
+
+    public double MaxSpeed
+    {
+      get
+      {
+        return this.maxSpeed;
+      }
+    }
+
+    public double MaxForce
+    {
+      get
+      {
+        return this.maxForce;
+      }
+    }
+
+    public double VisionAngle
+    {
+      get
+      {
+        return this.visionAngle;
+      }
+    }
+
+    public double VisionRadius
+    {
+      get
+      {
+        return this.visionRadius;
+      }
+    }
+
+    public int HistoryLength
+    {
+      get
+      {
+        return this.historyLength;
+      }
+    }
+
+    public Point3d Position 
+    {
+      get
+      {
+        return this.position;
+      }
+      set
+      {
+        this.position = value;
+      }
+    }
+
+    public Vector3d Velocity
+    {
+      get
+      {
+        return this.velocity;
+      }
+      set
+      {
+        this.velocity = value;
+      }
+    }
+
+    public Vector3d Acceleration
+    {
+      get
+      {
+        return this.acceleration;
+      }
+    }
+
+    public Point3d RefPosition
+    {
+      get
+      {
+        return this.refPosition;
+      }
+      set
+      {
+        this.refPosition = value;
+      }
+    }
+
+    public void update()
+    {
+      velocity = Vector3d.Add(velocity, acceleration);
+      refPosition.Transform(Transform.Translation(velocity));
+      position.Transform(Transform.Translation(velocity)); //So disconnecting the environment allows the agent to continue from its current position.
+      acceleration = Vector3d.Multiply(acceleration, 0);
+      lifespan -= 1;
+
+    }
+
+    public void applyForce(Vector3d force)
+    {
+      Vector3d f = force;
+      f = Vector3d.Divide(f, mass);
+      acceleration = Vector3d.Add(acceleration, f);
+    }
+
+    public Boolean isDead()
+    {
+      return (lifespan <= 0.0);
+    }
+
+    public void run()
+    {
+      update();
+    }
+
+    public override bool Equals(System.Object obj)
+    {
+      // If parameter is null return false.
+      if (obj == null)
+      {
+        return false;
+      }
+
+      // If parameter cannot be cast to Point return false.
+      AgentType p = obj as AgentType;
+      if ((System.Object)p == null)
+      {
+        return false;
+      }
+
+      // Return true if the fields match:
+      return this.acceleration.Equals(p.acceleration) &&
+             this.bodySize.Equals(p.bodySize) &&
+             this.historyLength.Equals(p.historyLength) &&
+             this.lifespan.Equals(p.lifespan) &&
+             this.position.Equals(p.position) &&
+             this.mass.Equals(p.mass) &&
+             this.maxForce.Equals(p.maxForce) &&
+             this.maxSpeed.Equals(p.maxSpeed) &&
+             this.velocity.Equals(p.velocity) &&
+             this.visionAngle.Equals(p.visionAngle) &&
+             this.visionRadius.Equals(p.visionRadius);
+    }
+
+    public bool Equals(AgentType p)
+    {
+      // If parameter is null return false:
+      if ((object)p == null)
+      {
+        return false;
+      }
+
+      // Return true if the fields match:
+      return this.acceleration.Equals(p.acceleration) &&
+             this.bodySize.Equals(p.bodySize) &&
+             this.historyLength.Equals(p.historyLength) &&
+             this.lifespan.Equals(p.lifespan) &&
+             this.position.Equals(p.position) &&
+             this.mass.Equals(p.mass) &&
+             this.maxForce.Equals(p.maxForce) &&
+             this.maxSpeed.Equals(p.maxSpeed) &&
+             this.velocity.Equals(p.velocity) &&
+             this.visionAngle.Equals(p.visionAngle) &&
+             this.visionRadius.Equals(p.visionRadius);
+    }
+
+    public override int GetHashCode()
+    {
+      // Return true if the fields match:
+      return this.acceleration.GetHashCode() ^
+             this.bodySize.GetHashCode() ^
+             this.historyLength.GetHashCode() ^
+             this.lifespan.GetHashCode() ^
+             this.position.GetHashCode() ^
+             this.mass.GetHashCode() ^
+             this.maxForce.GetHashCode() ^
+             this.maxSpeed.GetHashCode() ^
+             this.velocity.GetHashCode() ^
+             this.visionAngle.GetHashCode() ^
+             this.visionRadius.GetHashCode();
+    }
+
+    public override IGH_Goo Duplicate()
+    {
+      return new AgentType(this);
+    }
+
+    public override bool IsValid
+    {
+      get
+      {
+        return (lifespan > 0 && mass > 0 && bodySize >= 0 && maxForce >= 0 &&
+                maxSpeed >= 0 && visionAngle >= 0 && visionRadius >= 0 &&
+                historyLength >= 0);
+      }
+    }
+
+    public override string ToString()
+    {
+      string lifespan = "Lifespan: " + this.lifespan.ToString() + "\n";
+      string mass = "Mass: " + this.mass.ToString() + "\n";
+      string bodySize = "Body Size: " + this.bodySize.ToString() + "\n";
+      string maxForce = "Maximum Force: " + this.maxForce.ToString() + "\n";
+      string maxSpeed = "Maximum Speed: " + this.maxSpeed.ToString() + "\n";
+      string visionAngle = "Vision Angle: " + this.visionAngle.ToString() +
+                           "\n";
+      string visionRadius = "Vision Radius: " + this.visionRadius.ToString() +
+                            "\n";
+      string historyLength = "History Length: " + this.historyLength.ToString()
+                             + "\n";
+      return lifespan + mass + bodySize + maxForce + maxSpeed + visionAngle +
+             visionRadius + historyLength;
+    }
+
+    public override string TypeDescription
+    {
+      get { return "An Agent"; }
+    }
+
+    public override string TypeName
+    {
+      get { return "Agent"; }
+    }
+
+    
+  }
+}
