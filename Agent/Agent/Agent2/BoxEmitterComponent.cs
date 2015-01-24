cefbeff0997b6f75db55fef3ec4f74e263a4a998
Alex Fischer <lxfschr@gmail.com>
2015-01-24 00:54:38 -0500

    implemented separateForce

--- Agent/Agent/Agent2/BoxEmitterComponent.cs
+++ Agent/Agent/Agent2/BoxEmitterComponent.cs
@@ -50,7 +50,7 @@ namespace Agent
     {
       // Use the pManager object to register your output parameters.
       // Output parameters do not have default values, but they too must have the correct access type.
-      pManager.AddGenericParameter("Box Emitter", "B", "Box Emitter", GH_ParamAccess.item);
+      pManager.AddGenericParameter("Box Emitter", "E", "Box Emitter", GH_ParamAccess.item);
       //pManager.AddPointParameter("Point Emitter Location", "P", "Point Emitter Location", GH_ParamAccess.item);
 
       // Sometimes you want to hide a specific parameter from the Rhino preview.
