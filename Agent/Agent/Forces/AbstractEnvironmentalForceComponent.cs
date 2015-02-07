5d3e75b0588d2ae0e4ece75242926f490e136908
Alex Fischer <lxfschr@gmail.com>
2015-02-07 01:41:54 -0500

    implemented view force.

---
+++ Agent/Agent/Forces/AbstractEnvironmentalForceComponent.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using RS = Agent.Properties.Resources;
+
+namespace Agent
+{
+  public abstract class AbstractEnvironmentalForceComponent : GH_Component
+  {
+    protected readonly Bitmap icon;
+    protected readonly Guid componentGuid;
+    /// <summary>
+    /// Initializes a new instance of the ViewForceComponent class.
+    /// </summary>
+    protected AbstractEnvironmentalForceComponent(string name, string nickname, string description,
+                              string category, string subCategory, Bitmap icon, String componentGuid)
+      : base(name, nickname, description, category, subCategory)
+    {
+      this.icon = icon;
+      this.componentGuid = new Guid(componentGuid);
+    }
+
+    /// <summary>
+    /// Registers all the input parameters for this component.
+    /// </summary>
+    protected override void RegisterInputParams(GH_InputParamManager pManager)
+    {
+      // Use the pManager object to register your input parameters.
+      // You can often supply default values when creating parameters.
+      // All parameters must have the correct access type. If you want 
+      // to import lists or trees of values, modify the ParamAccess flag.
+      pManager.AddGenericParameter(RS.agentName, RS.agentNickName, RS.agentToAffect, GH_ParamAccess.item);
+      pManager.AddGenericParameter(RS.environmentName, RS.environmentNickName, RS.environmentDescription, GH_ParamAccess.item);
+      pManager.AddNumberParameter(RS.weightMultiplierName, RS.weightMultiplierNIckName, RS.weightMultiplierDescription,
+        GH_ParamAccess.item, RS.weightMultiplierDefault);
+      pManager.AddNumberParameter(RS.visionRadiusName, RS.visionRadiusNickName, RS.visionAngleDescription,
+        GH_ParamAccess.item, RS.visionRadiusDefault);
+    }
+
+    /// <summary>
+    /// Registers all the output parameters for this component.
+    /// </summary>
+    protected abstract override void RegisterOutputParams(GH_OutputParamManager pManager);
+
+    /// <summary>
+    /// This is the method that actually does the work.
+    /// </summary>
+    /// <param name="da">The DA object is used to retrieve from inputs and store in outputs.</param>
+    protected override void SolveInstance(IGH_DataAccess da)
+    {
+      // First, we need to retrieve all data from the input parameters.
+      // We'll start by declaring variables and assigning them starting values.
+      AgentType agent = new AgentType();
+      AbstractEnvironmentType environment = new AxisAlignedBoxEnvironmentType();
+      double weightMultiplier = RS.weightMultiplierDefault;
+      double visionRadius = RS.visionRadiusDefault;
+
+      // Then we need to access the input parameters individually. 
+      // When data cannot be extracted from a parameter, we should abort this method.
+      if (!da.GetData(0, ref agent)) return;
+      if (!da.GetData(1, ref environment)) return;
+      if (!da.GetData(2, ref weightMultiplier)) return;
+      if (!da.GetData(3, ref visionRadius)) return;
+
+      // We're set to create the output now. To keep the size of the SolveInstance() method small, 
+      // The actual functionality will be in a different method:
+
+      Vector3d force = Run(agent, environment, weightMultiplier, visionRadius);
+
+      // Finally assign the output parameter.
+      da.SetData(0, force);
+    }
+
+    protected Vector3d Run(AgentType agent, AbstractEnvironmentType environment, double weightMultiplier, double visionRadius)
+    {
+      Vector3d force = CalcForce(agent, environment, visionRadius);
+      Vector3d.Multiply(force, weightMultiplier);
+      agent.ApplyForce(force);
+      return force;
+    }
+
+    protected abstract Vector3d CalcForce(AgentType agent, AbstractEnvironmentType environment, double visionRadius);
+
+    /// <summary>
+    /// Provides an Icon for the component.
+    /// </summary>
+    protected override Bitmap Icon
+    {
+      get
+      {
+        //You can add image files to your project resources and access them like this:
+        // return Resources.IconForThisComponent;
+        return icon;
+      }
+    }
+
+    /// <summary>
+    /// Gets the unique ID for this component. Do not change this ID after release.
+    /// </summary>
+    public override Guid ComponentGuid
+    {
+      get { return componentGuid; }
+    }
+  }
+}
---
+++ Agent/Agent/Forces/ViewForceComponent.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using RS = Agent.Properties.Resources;
+
+namespace Agent
+{
+  public class ViewForceComponent : AbstractBoidForceComponent
+  {
+    /// <summary>
+    /// Initializes a new instance of the ViewForceComponent class.
+    /// </summary>
+    public ViewForceComponent()
+      : base(RS.viewForceName, RS.viewForceComponentNickName,
+          RS.viewForceDescription,
+          RS.pluginCategoryName, RS.boidForcesSubCategoryName, RS.icon_coheseForce, RS.viewForceGuid)
+    {
+    }
+
+    /// <summary>
+    /// Registers all the output parameters for this component.
+    /// </summary>
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+    {
+      // Use the pManager object to register your output parameters.
+      // Output parameters do not have default values, but they too must have the correct access type.
+      pManager.AddGenericParameter(RS.viewForceName, RS.forceNickName, 
+                                   RS.viewForceDescription, GH_ParamAccess.item);
+
+      // Sometimes you want to hide a specific parameter from the Rhino preview.
+      // You can use the HideParameter() method as a quick way:
+      //pManager.HideParameter(1);
+    }
+
+    protected override Vector3d CalcForce(AgentType agent, List<AgentType> neighbors)
+    {
+      Vector3d sum = new Vector3d();
+      int count = 0;
+      Vector3d steer = new Vector3d();
+      double angle = 0;
+      Point3d position = agent.Position;
+      Vector3d velocity = agent.Velocity;
+      Plane pl = new Plane(position, velocity, Vector3d.ZAxis);
+      foreach (AgentType neighbor in neighbors)
+      {
+        Vector3d diff = Vector3d.Subtract(new Vector3d(neighbor.Position), new Vector3d(position));
+        angle = Vector3d.VectorAngle(velocity, diff, pl);
+        angle = Util.Vector.RadToDeg(angle);
+        if (angle > 180) angle = angle - 360;
+        sum = Vector3d.Add(sum, new Vector3d(neighbor.Position));
+        //For an average, we need to keep track of how many boids
+        //are in our vision.
+        count++;
+      }
+
+      if (count > 0)
+      {
+        //We desire to go in that direction at maximum speed.
+        sum = Vector3d.Divide(sum, count);
+        Plane nrml = new Plane(new Point3d(position), velocity);
+        if (angle >= 0) sum.Rotate(Math.PI / 2, nrml.YAxis);
+        else sum.Rotate(-Math.PI / 2, nrml.YAxis);
+        //sum.PerpendicularTo(sum);
+        steer = Vector3d.Subtract(sum, velocity);
+        steer = Util.Vector.Limit(steer, agent.MaxForce);
+      }
+      //Seek the average location of our neighbors.
+      return steer;
+    }
+  }
+}
