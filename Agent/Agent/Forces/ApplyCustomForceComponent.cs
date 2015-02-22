c45d8cf7858f516fc14931ba13c842c89822c179
Alex Fischer <lxfschr@gmail.com>
2015-02-22 00:09:00 -0500

    added customForceComponent

---
+++ Agent/Agent/Forces/ApplyCustomForceComponent.cs
@@ -0,0 +1,38 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using RS = Agent.Properties.Resources;
+
+namespace Agent
+{
+  public class ApplyCustomForceComponent : AbstractForceComponent
+  {
+    private Vector3d vec;
+    public ApplyCustomForceComponent()
+      : base("Apply Custom Force", "ApplyForce",
+          "Applied a user specified force vector to the Agent.",
+          RS.pluginCategoryName, RS.forcesSubCategoryName, null, "{a6333370-2246-4fac-afb9-b858a809a414}")
+    {
+      vec = Vector3d.Zero;
+    }
+
+    protected override void RegisterInputParams2(GH_InputParamManager pManager)
+    {
+      pManager.AddVectorParameter("Force Vector", "V", "The vector to be applied to the Agent position.", GH_ParamAccess.item);
+    }
+
+    protected override bool GetInputs2(IGH_DataAccess da)
+    {
+      if (!da.GetData(nextInputIndex++, ref vec)) return false;
+      return true;
+    }
+
+    protected override Vector3d CalcForce()
+    {
+      return vec;
+    }
+
+    protected override void RegisterOutputParams2(GH_OutputParamManager pManager)
+    {
+    }
+  }
+}
