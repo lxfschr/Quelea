5d2d6454a85e6d52cc45a69e8174797771335f74
Alex Fischer <lxfschr@gmail.com>
2015-03-24 12:54:44 -0400

    added IAgent and IParticle interfaces.

---
+++ Agent/Agent/Agent/IAgent.cs
@@ -0,0 +1,13 @@
+﻿namespace Agent
+{
+  public interface IAgent : IParticle
+  {
+    double MaxSpeed { get; set; }
+
+    double MaxForce { get; set; }
+
+    double VisionRadius { get; set; }
+
+    double VisionAngle { get; set; }
+  }
+}
---
+++ Agent/Agent/Agent/IParticle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace Agent
+{
+  public interface IParticle : IGH_Goo, IPosition
+  {
+    Point3d Position { get; set; }
+    Vector3d Velocity { get; set; }
+    Vector3d Acceleration { get; set; }
+    Point3d RefPosition { get; set; }
+    int Lifespan { get; set; }
+
+    double Mass { get; set; }
+
+    double BodySize { get; set; }
+
+    int HistoryLength { get; set; }
+
+    bool InitialVelocitySet { get; set; }
+
+    void Kill();
+
+    CircularArray<Point3d> PositionHistory { get; }
+
+    List<Point3d> GetPositionHistoryList();
+
+    void Update();
+
+    void ApplyForce(Vector3d force);
+
+    Boolean IsDead();
+
+    void Run();
+
+    bool Equals(Object obj);
+
+    int GetHashCode();
+  }
+}
