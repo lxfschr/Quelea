d83774e785ced93900f3749560b87e4e489ca01d
Alex Fischer <lxfschr@gmail.com>
2015-04-23 15:07:06 -0400

    added fab file

--- fabfile.py
+++ fabfile.py
@@ -5,5 +5,4 @@ def git():
     #local('git config --global credential.helper "cache --timeout=3600"')
     message = raw_input("Enter a git commit message:  ")
     local("git add . && git add -u && git commit -m \"%s\"" % message)
-    local("git pull")
     local("git push")
