e11429600d7c7055d59cdd54e6e4a520c804bdc0
Alex Fischer <lxfschr@gmail.com>
2015-04-24 23:07:34 -0400

    implemented 2 versions of sense based on bitmap. need to figure out how to improve performance of simpler one.

---
+++ Quelea/Quelea/Actions/Forces/VehicleForces/SensorForceComponent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using RS = Quelea.Properties.Resources;
+
+namespace Quelea
+{
+  public class SensorForceComponent : AbstractVehicleForceComponent
+  {
+    private double sensorLeftValue, sensorRightValue;
+    private bool crossed;
+    public SensorForceComponent()
+      : base("Sensor Force", "SensorForce",
+          "Sensor Force",
+          null, "18b76cb8-408f-47b1-87f2-a0c0589c42dd")
+    {
+    }
+
+    protected override void RegisterInputParams(GH_InputParamManager pManager)
+    {
+      base.RegisterInputParams(pManager);
+      pManager.AddNumberParameter("SL", "SL", "SL", GH_ParamAccess.item);
+      pManager.AddNumberParameter("SR", "SR", "SR", GH_ParamAccess.item);
+      pManager.AddBooleanParameter("Crossed?", "C", "If true, the sensors will affect the wheels on the opposite side.",
+        GH_ParamAccess.item, false);
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+    {
+      base.RegisterOutputParams(pManager);
+      pManager.AddNumberParameter("SL", "SL", "SL", GH_ParamAccess.item);
+      pManager.AddNumberParameter("SR", "SR", "SR", GH_ParamAccess.item);
+    }
+
+    protected override bool GetInputs(IGH_DataAccess da)
+    {
+      if (!base.GetInputs(da)) return false;
+      if (!da.GetData(nextInputIndex++, ref sensorLeftValue)) return false;
+      if (!da.GetData(nextInputIndex++, ref sensorRightValue)) return false;
+      if (!da.GetData(nextInputIndex++, ref crossed)) return false;
+      return true;
+    }
+
+    protected override void SetOutputs(IGH_DataAccess da)
+    {
+      base.SetOutputs(da);
+      da.SetData(nextOutputIndex++, sensorLeftValue);
+      da.SetData(nextOutputIndex++, sensorRightValue);
+    }
+
+    protected override Vector3d CalcForce()
+    {
+      if (crossed)
+      {
+        vehicle.SetSpeedChanges(sensorRightValue, sensorLeftValue);
+      }
+      else
+      {
+        vehicle.SetSpeedChanges(sensorLeftValue, sensorRightValue);
+      }
+      
+      return Vector3d.Zero;
+    }
+  }
+}
